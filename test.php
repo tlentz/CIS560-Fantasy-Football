df4a1a83e676ec6e73fd65fa8054abe3a6d60a84
Markloev <markloev@gmail.com>
2016-11-06 00:33:39 -0500

    Database success

--- test.php
+++ test.php
@@ -1,3 +1,11 @@
 <?php
-echo "test";
+$mysqli = new mysqli("mysql.cis.ksu.edu", "markloev", "pcEkhG5B5kg8XExJ%RD");
+
+// Check connection
+if (!$mysqli) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully<br>";
+
+echo "Hello world!!!";
 ?>
